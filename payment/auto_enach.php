9b2619909608a5f996177feccd2133eb70627cdb
Atul Mishra <mishrapintu8800@gmail.com>
2025-09-21 11:56:39 +0530

    Enable error reporting in auto_enach.php: add configuration to display startup errors and set error reporting level to maximum. This change aims to improve debugging during cron job execution.

--- payment/auto_enach.php
+++ payment/auto_enach.php
@@ -1,4 +1,7 @@
 <!-- <?php
+ ini_set('display_startup_errors', 1);
+ ini_set('display_errors', 1);
+ error_reporting(-1);
 // Set a longer execution time limit, essential for cron jobs that might process many records.
 set_time_limit(0); 
 
