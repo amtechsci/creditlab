90d3431c602202ca56bac99993f7e681d2aa0980
Atul Mishra <mishrapintu8800@gmail.com>
2025-09-25 15:23:28 +0530

    Improve user registration logic in register.php by adding checks for assigned account and recovery officers. Implement fallback to default values when no assignments are found, enhancing robustness and preventing errors during user creation.

--- user/register.php
+++ user/register.php
@@ -20,14 +20,27 @@ if(isset($_POST['mobile'])){
     $rcid = "CL".date('ymd').$id;
     $reg_date = date('Y-m-d H:i:s');
     $aa = towquery("SELECT id FROM `account_manager`");
+$min = array();
 while($b = towfetch($aa)){
 $a = towquery("SELECT assign_account_manager FROM `user` WHERE assign_account_manager=".$b['id']."");
 $min[$b['id']] = townum($a);
-}$assign_account = array_keys($min, min($min));$assign_account = $assign_account[0];$aaz = towquery("SELECT id FROM `recovery_officer`");
+}
+if(!empty($min)){
+$assign_account = array_keys($min, min($min));$assign_account = $assign_account[0];
+}else{
+$assign_account = 0;
+}
+$aaz = towquery("SELECT id FROM `recovery_officer`");
+$minz = array();
 while($bz = towfetch($aaz)){
 $az = towquery("SELECT assign_recovery_officer FROM `user` WHERE assign_recovery_officer=".$bz['id']."");
 $minz[$bz['id']] = townum($az);
-}$assign_recovery = array_keys($minz, min($minz));$assign_recovery = $assign_recovery[0];
+}
+if(!empty($minz)){
+$assign_recovery = array_keys($minz, min($minz));$assign_recovery = $assign_recovery[0];
+}else{
+$assign_recovery = 0;
+}
 
 $a = towquery("INSERT INTO `user`(`rcid`, `mobile`, `active`, `verify`, `otp`, `validation`, `reg_date`, `status`, `document_password`, `loan_limit`, `assign_account_manager`, `assign_recovery_officer`, `approvenew`) VALUES ('$rcid','$mobile',0,0,$otp,'','$reg_date','waiting','pan no password pan#aadhar no password aadhar#aadha2 no password aadha2#salary no password salary#bank no password bank#address no password address#bank2 no password bank2#bank3 no password bank3',10000,$assign_account,$assign_recovery,0)");
 $message="$otp is OTP for Creditlab login verification & valid till 2min. Don't share this OTP with anyone.";
