24e164f72e0b958d2deaf7fa8308ae63d1138afd
Atul Mishra <mishrapintu8800@gmail.com>
2025-09-25 15:37:38 +0530

    Enhance user registration logic in login.php by implementing checks for assigned account and recovery officers. Introduce fallback values to prevent errors when no assignments are found, improving robustness during user creation.

--- account/home/login.php
+++ account/home/login.php
@@ -126,20 +126,30 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
     $otp = rand(1000,9999);
     #account_manager
     $aa = towquery("SELECT id FROM `account_manager`");
+$min = array();
 while($b = towfetch($aa)){
 $a = towquery("SELECT assign_account_manager FROM `user` WHERE assign_account_manager=".$b['id']."");
 $min[$b['id']] = townum($a);
 }
+if(!empty($min)){
 $assign_account = array_keys($min, min($min));
 $assign_account = $assign_account[0];
+}else{
+$assign_account = 0;
+}
 
 $aaz = towquery("SELECT id FROM `recovery_officer`");
+$minz = array();
 while($bz = towfetch($aaz)){
 $az = towquery("SELECT assign_recovery_officer FROM `user` WHERE assign_recovery_officer=".$bz['id']."");
 $minz[$bz['id']] = townum($az);
 }
+if(!empty($minz)){
 $assign_recovery = array_keys($minz, min($minz));
 $assign_recovery = $assign_recovery[0];
+}else{
+$assign_recovery = 0;
+}
     #account_manager
     
     $a = towquery("INSERT INTO `user`(`rcid`, `name`, `email`, `password`, `active`, `verify`, `otp`, `validation`, `reg_date`, `status`, `document_password`, `loan_limit`, `assign_account_manager`, `assign_recovery_officer`, `star_member`) VALUES ('$rcid','$name','$email','$password',1,0,$otp,'','$reg_date','waiting','pan no password pan#aadhar no password aadhar#aadha2 no password aadha2#salary no password salary#bank no password bank#address no password address#bank2 no password bank2#bank3 no password bank3',10000,$assign_account,$assign_recovery,2)");
